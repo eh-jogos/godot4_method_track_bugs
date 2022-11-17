68dc919d2d7cf3b30d38d43e2a8e42fa26aac8fc
Daniel Queiroz Porto <danielvandalk@gmail.com>
2022-11-17 16:56:28 -0300

    Add scene testing one shot node in AnimationTree

---
+++ anim_tree_blend_tree_one_shot.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+@onready var _animator := $AnimationPlayer as AnimationPlayer
+@onready var _anim_tree := $AnimationTree as AnimationTree
+
+var _use_anim_player := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_valid_path := false
+	var target_path = ""
+	
+	if event.is_action_pressed("ui_cancel"):
+		_use_anim_player = not _use_anim_player
+		if _use_anim_player:
+			_anim_tree.active = false
+		else:
+			_animator.stop()
+			_anim_tree.active = true
+			
+		print("Using Animation Player? %s"%[_use_anim_player])
+	
+	if event.is_action_pressed("ui_up"):
+		if _use_anim_player:
+			target_path = "method_at_4"
+		else:
+			target_path = "4frames"
+	elif event.is_action_pressed("ui_right"):
+		if _use_anim_player:
+			target_path = "method_at_5"
+		else:
+			target_path = "5frames"
+	elif event.is_action_pressed("ui_down"):
+		if _use_anim_player:
+			target_path = "method_at_6"
+		else:
+			target_path = "6frames"
+	elif event.is_action_pressed("ui_left"):
+		if _use_anim_player:
+			target_path = "method_at_0.25s"
+		else:
+			target_path = "0_25seconds"
+	
+	
+	if target_path != "":
+		print("playing %s"%[target_path])
+		if _use_anim_player:
+			_animator.play(target_path)
+		else:
+			_anim_tree["parameters/%s/active"%[target_path]] = true
+
+
+func print_i_worked() -> void:
+	print("I WORKED!")
